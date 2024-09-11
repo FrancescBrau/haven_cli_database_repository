11b47b4877584a4bea7fc8d9a3a1b8781aa2d155
Francesc Brau <frjosepgbrau@gmail.com>
2024-09-11 14:54:40 +0200

    solve errors

--- data/database_repository.dart
+++ data/database_repository.dart
@@ -1,9 +1,9 @@
-import '/models/usersList.dart'
+import 'file:///Users/francescbrau/Code/Haven-Project/Haven-Project%20DATABASE/haven_cli_database_repository/models/usersList.dart';
 
 abstract class DatabaseRepository {
   void addUser(String id, UsersList newUser);
-  
+
   List<String> getAllUserNames();
-  
+
   void removeUser(String id);
 }
--- data/mock_database.dart
+++ data/mock_database.dart
@@ -1,5 +1,5 @@
 import 'database_repository.dart';
-import '/models/usersList.dart';
+import 'file:///Users/francescbrau/Code/Haven-Project/Haven-Project%20DATABASE/haven_cli_database_repository/models/usersList.dart';
 
 class MockDatabase implements DatabaseRepository {
   Map<String, UsersList> userData = {};
@@ -7,18 +7,18 @@ class MockDatabase implements DatabaseRepository {
   @override
   void addUser(String id, UsersList newUser) {
     userData[id] = newUser;
-    print("User added: ${newUser.name}");
+    print("User added: ${newUser.userName}");
   }
 
   @override
   List<String> getAllUserNames() {
-    return userData.values.map((user) => user.name).toList();
+    return userData.values.map((user) => user.userName).toList();
   }
 
   @override
   void removeUser(String id) {
     if (userData.containsKey(id)) {
-      print("Delated user: ${userData[id]!.name}");
+      print("Delated user: ${userData[id]!.userName}");
       userData.remove(id);
     } else {
       print("ID user $id not found.");
