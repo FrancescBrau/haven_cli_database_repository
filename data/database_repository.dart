feb9c84ab9ba47296f566b318028c9ffb4baf21a
Francesc Brau <frjosepgbrau@gmail.com>
2024-09-11 15:25:27 +0200

    changes

--- data/database_repository.dart
+++ data/database_repository.dart
@@ -1,9 +1,13 @@
 import 'file:///Users/francescbrau/Code/Haven-Project/Haven-Project%20DATABASE/haven_cli_database_repository/models/usersList.dart';
 
 abstract class DatabaseRepository {
-  void addUser(String id, UsersList newUser);
+  void addUser(String email, UsersList newUser);
 
-  List<String> getAllUserNames();
+  List<UsersList> getAllUserNames();
+
+  UsersList getUserByEmail(String email);
+
+  void updateUsersStatus(String email, bool isActive);
 
   void removeUser(String id);
 }
--- data/mock_database.dart
+++ data/mock_database.dart
@@ -2,26 +2,52 @@ import 'database_repository.dart';
 import 'file:///Users/francescbrau/Code/Haven-Project/Haven-Project%20DATABASE/haven_cli_database_repository/models/usersList.dart';
 
 class MockDatabase implements DatabaseRepository {
-  Map<String, UsersList> userData = {};
+  Map<String, UsersList> usersList = {};
 
   @override
-  void addUser(String id, UsersList newUser) {
-    userData[id] = newUser;
-    print("User added: ${newUser.userName}");
-  }
-
-  @override
-  List<String> getAllUserNames() {
-    return userData.values.map((user) => user.userName).toList();
-  }
-
-  @override
-  void removeUser(String id) {
-    if (userData.containsKey(id)) {
-      print("Delated user: ${userData[id]!.userName}");
-      userData.remove(id);
+  void addUser(String userName, String email, bool isActive) {
+    if (!usersList.containsKey(email)) {
+      usersList[email] =
+          User(userName: userName, email: email, isActive: isActive);
     } else {
-      print("ID user $id not found.");
+      print('User with email $email already exists.');
+    }
+  }
+
+  @override
+  List<UsersList> getAllUserNames() {
+    // Return all users as a list
+    return usersList.values.toList();
+  }
+
+  @override
+  UsersList getUserByEmail(String email) {
+    // Get user details by email
+    if (usersList.containsKey(email)) {
+      return usersList[email]!;
+    } else {
+      print('User with email $email not found.');
+      return User(userName: 'Unknown', email: email, isActive: false);
+    }
+  }
+
+  @override
+  void updateUsersStatus(String email, bool isActive) {
+    // Update the active status of a user
+    if (usersList.containsKey(email)) {
+      usersList[email]!.isActive = isActive;
+    } else {
+      print('User with email $email not found.');
+    }
+  }
+
+  @override
+  void removeUser(String email) {
+    // Delete a user by email
+    if (usersList.containsKey(email)) {
+      usersList.remove(email);
+    } else {
+      print('User with email $email not found.');
     }
   }
 }
