6d29851533406a72ca69569ad8986eaef90e251e
Francesc Brau <frjosepgbrau@gmail.com>
2024-09-11 20:54:04 +0200

    commit1

--- main.dart
+++ main.dart
@@ -1,8 +1,24 @@
 import 'data/mock_database.dart';
+import 'models/user.dart';
 
 void main() {
   MockDatabase datenbank = MockDatabase();
 
   datenbank.addUser('JP', 'jp@app-akademie.com', false);
   datenbank.addUser('K.I.', 'kaihatje@app-akademie.com', false);
+  datenbank.addUser('Tabea', 'Tabeagnade@app-akademie.com', false);
+  datenbank.addUser('Francesc', 'francesc@gmail.com', true);
+
+  List<User> users = datenbank.getAllUserNames();
+  print(users.map((User) => User.userName).toList());
+
+  User user = datenbank.getUserByEmail('jp@app-akademie.com');
+  print('User: ${user.userName}, Active: ${user.isActive}');
+
+  datenbank.updateUsersStatus('jp@app-akademie.com', true);
+
+  datenbank.removeUser('tabeagnade@app-akademie.com');
+
+  users = datenbank.getAllUserNames();
+  print(users.map((user) => user.userName).toList());
 }
